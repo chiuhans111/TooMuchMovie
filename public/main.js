e627fed3acf7814fe67ab16ac5b9bf9c7564080f
chiuhans111 <chiuhans111@gmail.com>
2017-02-08 12:01:42 +0800

    bug

--- public/main.js
+++ public/main.js
@@ -2,7 +2,7 @@ var xhr = new XMLHttpRequest();
 xhr.open("post", "/cor");
 
 xhr.onload = function () {
-    console.log(this.responce);
+    console.log(this.response);
 }
 xhr.setRequestHeader("Content-Type", "application/json");
 xhr.send(JSON.stringify({ a: 1, b: 2 }));
